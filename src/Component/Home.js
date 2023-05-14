811d302473838c6152e45553a9712afd27311e80
Isah Jacob <isahjacob0@gmail.com>
2023-05-14 10:19:03 +0100

    corrected

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
 import Nav from "./Nav";
+import "./Home.css"
 import myImage from "../image/myimage.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./Home.css"
-import {
-    faTwitter,
-    faGithub,
-    faLinkedinIn
-  } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import Footer from "./Footer";
 
 
@@ -32,7 +28,7 @@ function Home(){
                         <p className="about-paragraph">I am helping others as I advance in my career by sharing my experiences and perspectives through writing. On my free time, I attend meetups, listen to engineering podcasts, read tech blogs, and take online courses to help me become a better developer for the people around me.</p>
                         <div className="social-button">
                             <a href="https://www.linkedin.com/in/isahejacob">
-                                <FontAwesomeIcon icon={faLinkedinIn} className="linkedin-Icon" size="2x" />
+                                <FontAwesomeIcon icon={faLinkedin} className="linkedin-Icon" size="2x" />
                             </a>
                             <a href="https://github.com/enecode" className="github-Icon">
                                 <FontAwesomeIcon icon={faGithub} size="2x" />
