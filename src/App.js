f10761c0ddfe388280988f2e5d9d5e2115656168
Isah Jacob <isahjacob0@gmail.com>
2023-05-14 09:33:10 +0100

    made corrections

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/project" element={<h1>Project</h1>} /> */}
         <Route path="/inspiration" element={<Inspiration />} />
-        <Route path='*' element={<h1>404 Not Found</h1>} />
       </Routes>
     </Router>
   );
