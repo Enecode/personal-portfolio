74fe449a43edb95b7051402098b7484afe10abf1
Isah Jacob <isahjacob0@gmail.com>
2023-10-31 23:24:22 +0100

    modified the Nav.js file

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -7,17 +7,34 @@ export default function Nav(){
     return(
         <div className="nav-container">
             <div className="content-container"> 
-                <button className="about">
-                <Link className="about-text" to='/about'>
-                About
-                </Link>
-                    
+                <button className="home">
+                    <Link className="about-text" to='/'>
+                        Home
+                    </Link>
                 </button>
                 
-                <button className="inspiration">
+                {/* <button className="about">
+                    <Link className="about-text" to='/about'>
+                        About
+                    </Link>
+                </button> */}
+
+                <button className="article">
+                    <Link className="about-text" to='/article'>
+                        Article
+                    </Link>
+                </button>
+
+                <button className="project">
+                    <Link className="about-text" to='/project'>
+                        Project
+                    </Link>
+                </button>
+                
+                {/* <button className="inspiration">
                 <Link className="about-text" to='/inspiration'>
                 Inspiration
-                </Link></button>
+                </Link></button> */}
             </div>
         </div>
     )
